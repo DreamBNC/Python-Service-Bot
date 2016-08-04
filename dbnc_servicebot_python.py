62ccdfcb5030596cbd0ec0f2d3abea3310beae41
S Stewart <xshotD@users.noreply.github.com>
2016-08-04 01:17:12 -0500

    Complete basic irc bot

--- dbnc_servicebot_python.py
+++ dbnc_servicebot_python.py
@@ -3,7 +3,28 @@
 # (c) DreamBNC
 
 # Loading essential libraries (needed for connecting to IRC)
-import socket, ssl
+import zirc, ssl
+
 
 print("DreamBNC Service Bot")
 print("(c) 2016 DreamBNC dev team.")
+print("Initalizing.")
+# setting variables
+bncprovider = DreamBNC
+bncweb = http://dreambnc.xyz
+server1 = Mushroom
+print("Initalized; connecting...")
+# connecting using zirc
+class Bot(zirc.Client):
+ def __init__(self):
+ self.connection = zirc.Socket(wrapper=ssl.wrap_socket) 
+self.config = zirc.IRCConfig(host="irc.freenode.net",
+ port=6697,
+ nickname="%s", % (bncprovider,)
+ ident="bnc",
+ realname="BNC request bot", 
+ channels [%s],  % (bncprovider,) 
+sasl_user="user", 
+sasl_pass="pw") 
+self.connect(self.config) 
+self.start()
